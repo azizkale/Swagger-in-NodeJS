1d4e4e1fab056b532388e799b6a4603d35e15efe
azizkale <azizkale@hotmail.com>
2021-02-02 05:40:38 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import postRouter from "../Routes/posts";
+
+const PORT = process.env.PORT || 8080;
+dotenv.config();
+
+const app = express();
+app.use(morgan());
+app.use(cors());
+
+app.use("/posts", postRouter);
+
+app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
