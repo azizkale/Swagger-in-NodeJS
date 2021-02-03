7ad83d850823f090df8ef800252604eb5dd58507
azizkale <azizkale@hotmail.com>
2021-02-03 11:43:24 +0100

    swggerConfiguration,added

--- Routes/posts.js
+++ Routes/posts.js
@@ -5,9 +5,88 @@ import bodyParser from "body-parser";
 const postRouter = express.Router();
 postRouter.use(bodyParser.json()); // to use body object in requests
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - userId
+ *         - title
+ *         - body
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The Auto-generated id of a post
+ *         userId:
+ *           type: integer
+ *           description: id of author
+ *         title:
+ *           type: string
+ *           description: title of post
+ *         body:
+ *           type: string
+ *           descripton: content of post *
+ *       example:
+ *         id: 1
+ *         userId: 1
+ *         title: my title
+ *         body: my article
+ *
+ */
+
+/**
+ * @swagger
+ *  tags:
+ *    name: Posts
+ *    description: posts of users
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Returns all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: the list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
+
 postRouter.get("/", (req, res) => {
   res.send(data);
 });
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: gets posts by id
+ *     tags: [Posts]
+ *     parameters:
+ *       - in : path
+ *         name: id
+ *         description: id of post
+ *         schema:
+ *           type: integer
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: posts by its id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       400:
+ *         description: post can not be found
+ */
 
 postRouter.get("/:id", (req, res) => {
   const post = data.find((post) => post.id === +req.params.id);
@@ -19,11 +98,34 @@ postRouter.get("/:id", (req, res) => {
   res.send(post);
 });
 
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: The post was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       500:
+ *         description: Some server error
+ */
+
 postRouter.post("/", (req, res) => {
   try {
     const post = {
-      id: data.length + 1,
       ...req.body,
+      id: data.length + 1,
     };
 
     data.push(post);
@@ -34,18 +136,83 @@ postRouter.post("/", (req, res) => {
   }
 });
 
-postRouter.put("/:id", (req, res) => {
-  let post = data.find((post) => (post.id = req.params.id));
-  post = req.body;
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: updates posts by id
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: post id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         decsription: The post was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: post was not found.
+ *       500:
+ *         description: Some errors happend.
+ *
+ */
 
-  res.end(JSON.stringify(post) + "updated");
+postRouter.put("/:id", (req, res) => {
+  try {
+    let post = data.find((post) => post.id === +req.params.id);
+    post.userId = req.body.userId;
+    post.title = req.body.title;
+    post.body = req.body.body;
+
+    res.send(post);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
 
+/**
+ * @swagger
+ *  /posts/{id}:
+ *    delete:
+ *      summary: removes post from array
+ *      tags: [Posts]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: post id
+ *          required: true
+ *          schema:
+ *            type: integer
+ *      responses:
+ *        200:
+ *          description: The post was deleted
+ *        404:
+ *          description: The post was not found
+ *
+ */
+
 postRouter.delete("/:id", (req, res) => {
-  let post = data.find((post) => (post.id = req.params.id));
+  let post = data.find((post) => post.id === +req.params.id);
   const index = data.indexOf(post);
-  data.splice(index, 1);
-  res.send(data);
+
+  if (post) {
+    data.splice(index, 1);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 module.exports = postRouter;
--- src/index.js
+++ src/index.js
@@ -3,19 +3,36 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import postRouter from "../Routes/posts";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const PORT = process.env.PORT || 4001;
 dotenv.config();
 
 const app = express();
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
 
-app.use(morgan());
+app.use(morgan("dev"));
 app.use(cors());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      {
+        url: "http://localhost:4001",
+      },
+    ],
+  },
+  apis: ["./Routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use("/posts", postRouter);
 
