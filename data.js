02e7933fd949dc3f82511bf1ecd411abb7fd7fda
azizkale <64408361+azizkale@users.noreply.github.com>
2021-02-11 15:25:52 +0100

    Update data.js

--- data.js
+++ data.js
@@ -20,6 +20,6 @@ const data = [
     title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
     body:
       "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-  }
+  },
 ];
 module.exports = data;
