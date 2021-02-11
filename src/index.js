9689c8082728070af3814e2498da53e68866eb57
azizkale <64408361+azizkale@users.noreply.github.com>
2021-02-11 15:25:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,10 +21,18 @@ const options = {
       title: "Library API",
       version: "1.0.0",
       description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
     },
+
     servers: [
       {
         url: "http://localhost:4001",
+        description: "My API Documentation",
       },
     ],
   },
